5751f183e522a4630a42def2bd8bd94f0c850dcf
Hiroto Shishido <shishidohiroto@gmail.com>
2017-07-09 12:49:57 +0900

    commit trial

---
+++ lib/generators/active_record/templates/remove_merit_fields_from_model.rb
@@ -0,0 +1,6 @@
+class RemoveMeritFieldsFrom<%= table_name.camelize %> < ActiveRecord::Migration
+  def self.up
+    remove_column :<%= table_name %>, :sash_id
+    remove_column :<%= table_name %>, :level
+  end
+end
